15aaa68655931556429bb80090858fc47576345b
Jim T <jimte145@student.liu.se>
2025-05-29 14:47:54 +0200

    Add project files.

---
+++ QuickRef/QuickRef/Models/UserNote.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+namespace QuickRef.Models;
+
+public class UserNote(string title, string content)
+{
+    public string Title { get; set; } = title;
+
+    public string Content { get; set; } = content;
+
+    public DateTime Created { get; set; } = DateTime.Now;
+
+    // Tags
+}
