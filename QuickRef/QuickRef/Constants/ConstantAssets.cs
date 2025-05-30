3734f44ea6f43cb588288e7ee2d0c71eb3ea2110
Jim T <jimte145@student.liu.se>
2025-05-30 18:33:37 +0200

    Changes

---
+++ QuickRef/QuickRef/Constants/ConstantAssets.cs
@@ -0,0 +1,14 @@
+﻿namespace QuickRef.Constants;
+
+public static class ConstantAssets
+{
+    public static readonly string[] NoteQuotes =
+    [
+        "Notes, notes, notes...",
+        "A note a day keeps the forgetfulness away.",
+        "A note is worth a thousand words or smth..",
+        "In the world of notes, clarity is king.",
+        "Fireball!",
+        "Wow, such empty. Very editable.",
+    ];
+}
